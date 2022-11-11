32af0967f53155f06eb1f4b8b723076abe3f11d2
Nbdyleto <nobodyleto2004@gmail.com>
2022-11-11 20:29:39 -0300

    [daily task] adding row clicked functions

--- functions/new_daily_task/dailytask.py
+++ functions/new_daily_task/dailytask.py
@@ -6,27 +6,38 @@ from functions.new_daily_task.ui_daily_task import Ui_DailyTaskPage
 class DTaskMainPage(QtWidgets.QWidget):
     def __init__(self):
         super(DTaskMainPage, self).__init__()
-        
         self.ui = Ui_DailyTaskPage()
         self.ui.setupUi(self)
+        self.setupVariables()
+        self.setupConnections()
+        self.setupWidgets()
 
+    def setupVariables(self):
         global widgets
         widgets = self.ui
 
+    def setupConnections(self):
+        widgets.tblTasks.cellClicked.connect(self.rowClickedFunctions)
+
+    def setupWidgets(self):
         widgets.tblTasks.setColumnWidth(0,350)
         widgets.tblTasks.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
         widgets.tblTasks.horizontalHeader().setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
         widgets.tblTasks.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
         widgets.tblTasks.horizontalHeader().setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
-        widgets.tblTasks.setColumnWidth(5,40)
+        widgets.tblTasks.horizontalHeader().setSectionResizeMode(5, QtWidgets.QHeaderView.Stretch)
         widgets.tblTasks.setStyleSheet("""
             QTableWidget::item{
                 margin-top: 3px;          
-                border-radius: 0px;
                 padding-left: 15px;
                 text-align: center;
             }
         """)
+        self.hideAll()
+
+        widgets.tblLists.setVisible(False)
+        widgets.tblLists.setColumnCount(1)
+        widgets.tblLists.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
 
     def loadDataInTable(self):
         widgets.tblTasks.clearContents()
@@ -37,7 +48,6 @@ class DTaskMainPage(QtWidgets.QWidget):
             #tblTopics.setRowCount(topicscount+1)
             topics = db.cursor.execute("SELECT * FROM topics").fetchall()
             tasks = db.cursor.execute("SELECT * FROM tasks ORDER BY start_date").fetchall()
-
         try:
             tablerow = 0
             for row in tasks:
@@ -53,3 +63,68 @@ class DTaskMainPage(QtWidgets.QWidget):
             widgets.tblTasks.setRowHeight(tablerow, 50)
         except Exception:
             print('ERROR')
+    
+    # Row functions
+
+    def rowClickedFunctions(self, row, col):
+        if self.isExistentInDB(row) and col != 0:
+            widgets.lblSetInfo.setVisible(True)
+            if col == 1:
+                self.loadStatusList()
+            elif col == 2:
+                self.showInitialDate()
+            elif col == 3:
+                self.showEndDate()
+            elif col == 4:
+                self.loadTopicsList()
+            else: 
+                self.hideAll()
+
+    def isExistentInDB(self, row):
+        self.hideAll()
+        with DBMainOperations() as db:
+            try:
+                selectedrow = widgets.tblTasks.item(row, 0).text()
+                db.getAllRecords(tbl='tasks', specifcols='task_name', whclause=f'task_name = "{selectedrow}"')
+                print('existent!')
+                return True
+            except Exception:
+                print('not existent!')
+                return False
+
+    def hideAll(self):
+        widgets.tblLists.clearContents()
+        widgets.qCalendar.clearMask()
+        widgets.lblSetInfo.setVisible(False)
+        widgets.qCalendar.setVisible(False)
+        widgets.tblLists.setVisible(False)
+        widgets.lblSetInfo.setVisible(False)
+
+    def loadStatusList(self):
+        widgets.lblSetInfo.setText('Status:')
+        widgets.tblLists.show()
+        widgets.tblLists.setRowCount(3)
+        widgets.tblLists.setItem(0, 0, QtWidgets.QTableWidgetItem('Não iniciada.'))
+        widgets.tblLists.setItem(1, 0, QtWidgets.QTableWidgetItem('Em progresso...'))
+        widgets.tblLists.setItem(2, 0, QtWidgets.QTableWidgetItem('Concluida!'))
+    
+    def loadTopicsList(self):
+        widgets.tblLists.show()
+        widgets.lblSetInfo.setText('Tópico:')
+        with DBMainOperations() as db:
+            topicscount = db.cursor.execute("SELECT COUNT(*) FROM topics").fetchone()[0]
+            widgets.tblLists.setRowCount(topicscount)
+            topics = db.getAllRecords(tbl='topics')
+            print(f'topics: {topics}')
+        tablerow = 0
+        for row in topics:
+            widgets.tblLists.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[1]))
+            tablerow += 1
+
+    def showInitialDate(self):
+        widgets.lblSetInfo.setText('Data Inicial:')
+        widgets.qCalendar.show()
+
+    def showEndDate(self):
+        widgets.lblSetInfo.setText('Data Final:')
+        widgets.qCalendar.show()
